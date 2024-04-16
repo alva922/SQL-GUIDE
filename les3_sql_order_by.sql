e6c7572fc1f5309c35e9c36d7bb4b1fbe94d7421
Al Va <alexzap922@gmail.com>
2024-04-16 22:30:46 +0200

    Create les3_sql_order_by.sql
    SQL ORDER BY Statement

---
+++ les3_sql_order_by.sql
@@ -0,0 +1,9 @@
+/*https://newdigitals.org/2023/10/12/sql-cheat-sheets-interview-qa/#sql-order-by-statement*/
+SELECT * FROM Products
+ORDER BY Price;
+SELECT * FROM Products
+ORDER BY Price DESC;
+SELECT * FROM Products
+ORDER BY ProductNaSELECT * FROM Customers
+ORDER BY Country ASC, CustomerName DESC;
+
