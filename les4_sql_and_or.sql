f24c267c91d3f5a174b7d6c943a936b965982fd6
Al Va <alexzap922@gmail.com>
2024-04-22 14:30:28 +0200

    Update les4_sql_and_or.sql
    The SQL AND/OR Operator

--- les4_sql_and_or.sql
+++ les4_sql_and_or.sql
@@ -1,5 +1,5 @@
 /*https://newdigitals.org/2023/10/12/sql-cheat-sheets-interview-qa/#the-sql-and-or-operator*/
-
+--  The SQL AND/OR Operator
 /*Customers table: Select all customers from Spain that starts with the letter ‘G’*/
 SELECT * FROM Customers
 WHERE Country = 'Spain' AND CustomerName LIKE 'G%';
