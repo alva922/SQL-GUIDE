a3eefdc828b77716131b67a3f8e67478563f7980
Al Va <alexzap922@gmail.com>
2024-04-22 14:29:56 +0200

    Create les4_sql_and_or.sql
    The SQL AND/OR Operator

---
+++ les4_sql_and_or.sql
@@ -0,0 +1,18 @@
+/*https://newdigitals.org/2023/10/12/sql-cheat-sheets-interview-qa/#the-sql-and-or-operator*/
+
+/*Customers table: Select all customers from Spain that starts with the letter ‘G’*/
+SELECT * FROM Customers
+WHERE Country = 'Spain' AND CustomerName LIKE 'G%';
+
+-- Customers table: Select all Spanish customers that starts with either “G” or “R”
+SELECT * FROM Customers
+WHERE Country = 'Spain'
+AND (CustomerName LIKE 'G%' OR CustomerName LIKE 'R%');
+
+-- Customers table: Select all customers from Germany or Spain
+
+SELECT *
+FROM Customers
+WHERE Country = 'Germany' OR Country = 'Spain';
+
+
